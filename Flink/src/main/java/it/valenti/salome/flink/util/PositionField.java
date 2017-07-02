2003a40ee77b336eadb56b6646c62b9dc96d923b
alexiey91 <alexiey@hotmail.it>
2017-07-02 13:18:30 +0200

    GetPosition

---
+++ Flink/src/main/java/it/valenti/salome/flink/util/PositionField.java
@@ -0,0 +1,30 @@
+package main.java.it.valenti.salome.flink.util;
+
+import java.text.DecimalFormat;
+
+/**
+ * Created by root on 02/07/17.
+ */
+public class PositionField {
+    private double X_lenght= 52483;
+    private double Y_lenght = 67925;
+    private  double x_cell = 6560.375;
+    private double y_cell = 5225;
+
+    //traslo la y della metà di Y_lenght
+    // converto le x in abs se negative
+    public PositionField() {
+    }
+
+    public String positionCell(Double x, Double y){
+    String cella="";
+    if(x <0) Math.abs(x);
+    //valore per il calcolo della posizione della cella in y risperro il vertice in basso a sinistra
+    double adjustY= (Y_lenght/2)+y;
+    double cellX = Math.round(x/x_cell);
+    double cellY = Math.round(adjustY/y_cell);
+        DecimalFormat format = new DecimalFormat();
+    cella="("+format.format(cellX)+"-"+format.format(cellY)+")";
+    return cella;}
+
+}
